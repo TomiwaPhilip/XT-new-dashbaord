7335882733970cb4ee45040dd797f56afe26e0bd
TomiwaPhilip <tomiwaphilip1100@gmail.com>
2024-03-21 11:06:20 +0100

    updated

--- xt-dashboard/app/page.tsx
+++ xt-dashboard/app/page.tsx
@@ -42,24 +42,24 @@ export default function Home() {
         <div className="text-center text-[#00234E] text-[32px] px-10">Congratulations! You have been selected for the 
             3 months Software Engineering Mentorship Programme.
         </div>
-        <div className="mt-[80px] rounded-md text-[#131216] bg-[#4BA3C5] px-5">
-          <p className="text-[18px] font-bold text-center">Below is what you need to do now!</p>
-
-          <ol>
-            <li>
-              Join our discord community by clicking <a href=""> here. </a> This is your virtual workingspace for the mentorship programme.
-            </li>
-            <li>
-              Wait till your programme starts by April 15, 2024. (An email will be sent to you before then).
-            </li>
-            <li>
-              Start Preparing by setting up your dev tools!
-            </li>
-            <li>
-              Send us a message <a href="mailto:tomiwaphilip@xperiencedtekie.pro"> here. </a> telling us how happy you are!
-            </li>
-          </ol>
-
+        <div className="flex flex-col items-center justify-center h-full mt-[80px]">
+          <div className="rounded-md text-[#131216] bg-[#4BA3C5] px-5 py-8">
+            <p className="text-[18px] font-bold text-center mb-4">Below is what you need to do now!</p>
+            <ol className="list-decimal">
+              <li className="mb-2">
+                Join our discord community by clicking <a href=""> here </a>. This is your virtual workingspace for the mentorship programme.
+              </li>
+              <li className="mb-2">
+                Wait till your programme starts by April 15, 2024. (An email will be sent to you before then).
+              </li>
+              <li className="mb-2">
+                Start Preparing by setting up your dev tools!
+              </li>
+              <li>
+                Send us a message <a href="mailto:tomiwaphilip@xperiencedtekie.pro"> here </a> telling us how happy you are!
+              </li>
+            </ol>
+          </div>
         </div>
       </div>
     );
