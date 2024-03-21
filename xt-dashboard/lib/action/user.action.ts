52b640e871e4d8deacb30b2cbcf77d3d2ec016cd
TomiwaPhilip <tomiwaphilip1100@gmail.com>
2024-03-21 10:13:50 +0100

    updated

---
+++ xt-dashboard/lib/action/user.action.ts
@@ -0,0 +1,39 @@
+import connectToDB from "../database/database";
+import PaymentStatus from "../model/paymentstatus";
+import User from "../model/user";
+
+export async function payment({ email }: { email: string }): Promise<boolean> {
+  try {
+    await connectToDB();
+    const user = await PaymentStatus.findOne({ email });
+    return !!user?.paid; // Return false if user is null or paid is false, true otherwise
+  } catch (error) {
+    console.error("Error retrieving payment status:", error);
+    throw new Error("Failed to retrieve payment status");
+  }
+}
+
+export async function saveUser(email: string): Promise<boolean> {
+  try {
+    await connectToDB();
+    await User.create({
+      email: email,
+      onboarded: true,
+    });
+    return true;
+  } catch (error) {
+    console.error("Error saving user:", error);
+    throw new Error("Failed to save user");
+  }
+}
+
+export async function onboardStatus(email: string): Promise<boolean> {
+  try {
+    await connectToDB();
+    const user = await User.findOne({ email });
+    return !!user?.onboarded;
+  } catch (error) {
+    console.error("Error retrieving onboarding status:", error);
+    throw new Error("Failed to retrieve onboarding status");
+  }
+}
