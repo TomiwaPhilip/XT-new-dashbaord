a7729d9447d90caf8e0114f76b1d99ad404df97b
TomiwaPhilip <tomiwaphilip1100@gmail.com>
2024-03-21 18:34:05 +0100

    updated

--- xt-dashboard/components/shared.tsx
+++ xt-dashboard/components/shared.tsx
@@ -12,7 +12,7 @@ export function Nav() {
   return (
     <nav>
       <div className="fixed top-0 left-0 right-0 z-10 bg-[#00234E] flex items-center justify-between px-4 py-2">
-        <div className="text-white text-left">Welcome, `${user_firstName}`</div>
+        <div className="text-white text-left">Welcome, {user_firstName}</div>
         <div className="text-white text-right">
           <UserButton />
         </div>
