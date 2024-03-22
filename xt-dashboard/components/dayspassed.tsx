b4a62fbb51432e849255296caf63877dd45a2af7
TomiwaPhilip <tomiwaphilip1100@gmail.com>
2024-03-22 20:24:52 +0100

    updated

--- xt-dashboard/components/dayspassed.tsx
+++ xt-dashboard/components/dayspassed.tsx
@@ -5,13 +5,15 @@ import PDFGenerator from "@/components/handlepdf";
 export default function DaysPass() {
     return (
         <div className="mt-[120px] text-center">
-        <div className="text-center text-[#00234E] text-[32px] px-10 font-bold">Congratulations! You have been selected for the 
-            3 months Software Engineering Mentorship Programme.
-        </div>
-        <div className="flex items-center border-4 border-[#00234E] p-10 rounded-md justify-center w-[35%] h-full mt-[80px]">
-          <p className="text-[#00234E] text-[32px] pb-10 text-center font-bold">Download Certificate</p>
-          <PDFGenerator />
-        </div>
+            <div className="text-center text-[#00234E] text-[32px] px-10 font-bold">Congratulations! You have been selected for the 
+                3 months Software Engineering Mentorship Programme.
+            </div>
+            <div className="flex flex-col items-center justify-center h-screen">
+                <div className="flex flex-col items-center border-4 border-[#00234E] p-10 rounded-md w-[35%] mt-[80px]">
+                    <p className="text-[#00234E] text-[32px] pb-10 text-center font-bold">Download Certificate</p>
+                    <PDFGenerator />
+                </div>
+            </div>
         </div>
     )
 }
